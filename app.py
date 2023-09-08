b5902969d3ac8c31c7d9a9bcd320ec730e3753f6
watalog <144313975+watalog@users.noreply.github.com>
2023-09-08 16:05:36 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+# 以下を「app.py」に書き込み
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API Keyを取得
+openai.api_key = st.secret.OpenAIAPI.openai_api_key
+
+system_prompt = """
+あなたは優秀な英語講師です。
+英作文や英会話、リスニングやリーディングなど、ユーザーの要望に合わせて英語の上達のためにアドバイスを行ってください。
+あなたの役割はユーザーの英語力を向上させることなので、例えば以下のような英語以外のことを聞かれても、絶対に答えないでください。
+
+* 旅行
+* 料理
+* 芸能
+* 映画
+* 科学
+* 歴史
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = "" # 入力欄を消去
+
+# ユーザーインターフェイスの構築
+st.title(" 「英語講師」ボット")
+st.image("03_english.png")
+st.write("Let's enjoy English!")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]): # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
